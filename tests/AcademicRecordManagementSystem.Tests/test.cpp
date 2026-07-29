f931d5840985efaa0d4c64fa68c0a7dc6c7fa420
ArUser <User@LIBRARY-7>
2026-07-29 15:14:58 +0400

    Add first Student unit test

--- tests/AcademicRecordManagementSystem.Tests/test.cpp
+++ tests/AcademicRecordManagementSystem.Tests/test.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
+#include "Student.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST
+(
+	StudentTests,
+	ParameterizedConstructorStoresValidValues
+)
+{
+	Student student(1, "Arame", 95.5);
+
+	EXPECT_EQ(student.getId(), 1);
+	EXPECT_EQ(student.getName(), "Arame");
+	EXPECT_DOUBLE_EQ(student.getScore(), 95.5);
 }
